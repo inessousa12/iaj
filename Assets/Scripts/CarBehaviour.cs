570b7a1e4b01c2c646d3a87d5f9c8d3eafafb739
Ines Sousa <fc51588@alunos.fc.ul.pt>
2021-06-18 14:38:29 +0100

    car ai

---
+++ Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarBehaviour : MonoBehaviour
+{
+
+    CharacterNavigationController CarController;
+
+    [Header("Sensors")]
+    public float sensorLength = 3f;
+    public Vector3 frontSensorPosition = new Vector3(0f, 0.2f, 0.5f);
+    public float frontSideSensorPosition = 0.2f;
+    public float frontSensorAngle = 30f;
+
+
+    private void Start()
+    {
+        CarController = GameObject.Find("SportCar20").GetComponent<CharacterNavigationController>();
+    }
+
+    private void FixedUpdate()
+    {
+        Sensors();
+    }
+
+    private void Sensors()
+    {
+        RaycastHit hit;
+        Vector3 sensorStartPos = transform.position + frontSensorPosition;
+        sensorStartPos += transform.forward * frontSensorPosition.z;
+        sensorStartPos += transform.up * frontSensorPosition.y;
+
+        CarController.movementSpeed = 5;
+
+        //front center sensor
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            CarController.movementSpeed = 0;
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+
+        //front right sensor
+        sensorStartPos.x += frontSideSensorPosition;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            CarController.movementSpeed = 0;
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+
+        //front right angle sensor
+        if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength))
+        {
+            CarController.movementSpeed = 0;
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+
+        //front left sensor
+        sensorStartPos.x -= 2 * frontSideSensorPosition;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            CarController.movementSpeed = 0;
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+
+        //front left angle sensor
+        if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(-frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength))
+        {
+            CarController.movementSpeed = 0;
+            Debug.DrawLine(sensorStartPos, hit.point);
+        }
+
+    }
+}
